c1a56f8cca610599041546481ffc2058a1a023b1
bluboyd <joyousjoy2b2g@gmail.com@>
2024-07-24 21:54:01 +0100

    foundation2

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Reflection.Metadata.Ecma335;
+public class Product
+{
+    private string _name;
+    private int _productId;
+    private decimal _price;
+    private int _quantity;
+    public Product(string name, int productId, decimal price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+    public decimal TotalCost()
+    {
+        return _price * _quantity;
+    }
+    public string Name
+    {
+        get{return _name;}
+
+    }
+    public int ProductId
+    {
+        get {return _productId;}
+    }
+    public decimal Price
+    {
+        get{return _price;}
+    }
+    public int quantity
+    {
+        get {return _quantity;}
+    }
+}
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address1 = new Address ("123 main street", "Holywood", "CA", "USA");
+        Address address2 = new Address ("456 Abel Street", "Ikoyi", "LAG", "Nigeria");
+        Customer customer1 = new Customer ("Joseph Smith", address1);
+        Customer customer2 = new Customer ("Brigham Young", address2);
+
+        Product product1 = new Product ("Laptop", 1001, 9.00m, 2);
+        Product product2 = new Product ("IPhone", 1002, 14.50m, 3);
+        Product product3 = new Product ("Television", 1003, 12.00m, 1);
+
+        Order order1 = new Order (customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
+        Console.WriteLine("order 1: ");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total cost: {order1.TotalCost():F2}");
+
+        Console.WriteLine();
+
+        Console.WriteLine("order 2: ");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total cost: {order2.TotalCost():F2}");
     }
 }
